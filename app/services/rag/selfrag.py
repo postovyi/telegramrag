6e51dd8abcb2619b8469eb3b87f538776d9f7522
alex <o.postovyi@gmail.com>
2026-08-27 21:00:30 +0300

    refactor(rag): use RAGStrategy base-class helpers in SelfRAGStrategy

--- app/services/rag/selfrag.py
+++ app/services/rag/selfrag.py
@@ -1,19 +1,20 @@
+import instructor
 from atomic_agents import AgentConfig, AtomicAgent, BasicChatInputSchema
 from atomic_agents.context import SystemPromptGenerator
+
 from app.core.config import settings
-from app.models.telegram import TelegramChannel
+from app.prompts import OUTPUT_INSTRUCTIONS, SELF_RAG_PROMPT, SYSTEM_PROMPT
 from app.repository import TelegramPostMediaRepository, TelegramPostRepository
 from app.schemas import SelfRAGOutputSchema, TelegramPostSchema
+from app.services.rag.base import RAGStrategy
 from app.services.rag.embeddings import EmbeddingService
-from app.prompts import SYSTEM_PROMPT, SELF_RAG_PROMPT, OUTPUT_INSTRUCTIONS
-import instructor
-import numpy as np
 
 
-class SelfRAGStrategy:
-    def __init__(self, post_repository: TelegramPostRepository, post_media_repository: TelegramPostMediaRepository) -> None:
-        self.post_repository = post_repository
-        self.post_media_repository = post_media_repository
+class SelfRAGStrategy(RAGStrategy):
+    def __init__(
+        self, post_repository: TelegramPostRepository, post_media_repository: TelegramPostMediaRepository
+    ) -> None:
+        super().__init__(post_repository, post_media_repository)
         self.agent_config = AgentConfig(
             client=instructor.from_provider(
                 model=settings.rag.llm_model,
@@ -21,8 +22,10 @@ class SelfRAGStrategy:
                 mode=instructor.Mode.JSON,
                 base_url=settings.rag.llm_base_url,
             ),
-            model=settings.rag.llm_model.split("/", 1)[-1],
-            system_prompt_generator=SystemPromptGenerator(background=[SYSTEM_PROMPT], output_instructions=[OUTPUT_INSTRUCTIONS]),
+            model=settings.rag.llm_model.split('/', 1)[-1],
+            system_prompt_generator=SystemPromptGenerator(
+                background=[SYSTEM_PROMPT], output_instructions=[OUTPUT_INSTRUCTIONS]
+            ),
         )
         self.agent = AtomicAgent[BasicChatInputSchema, SelfRAGOutputSchema](self.agent_config)
 
@@ -32,21 +35,7 @@ class SelfRAGStrategy:
         )
 
         embedding_text = await EmbeddingService.embed_text(refined_query.query)
-        if media:
-            embedding_media = await EmbeddingService.embed_image(media)
-        else:
-            embedding_media = np.zeros(settings.rag.embedding_n_dim)
-
-        post_embedding = embedding_text + embedding_media
+        post_embedding = await self._build_query_embedding(embedding_text, media)
 
         posts = await self.post_repository.find_by_embedding(post_embedding.tolist())
-        return [
-            TelegramPostSchema(
-                id=post.id,
-                content=post.content,
-                posted_at=post.posted_at,
-                channel_url=(await self.post_repository.session.get(TelegramChannel, post.channel_id)).url,
-                url=post.url,
-            )
-            for post in posts
-        ]
+        return await self._to_post_schemas(posts)
